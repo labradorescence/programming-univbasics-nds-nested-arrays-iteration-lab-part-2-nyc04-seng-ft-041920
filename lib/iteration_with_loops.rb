eb5fd056a2a007a5bf303d5bde7120d43592e5e0
Greem <orangenewyorkcreativeagency@gmail.com>
2020-05-01 15:48:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,6 +4,7 @@ def find_min_in_nested_arrays(src)
   counter = 0
   min = 100
   arr = []
+  
   while counter < src.length do
     puts " src[cou] #{src[counter]} #{counter}"
 
@@ -12,11 +13,12 @@ def find_min_in_nested_arrays(src)
       puts "src [][] #{src[counter][inner_counter]}"
       if min > src[counter][inner_counter]
       min = src[counter][inner_counter]
-      puts "min #{arr.push(min)} #{inner_counter}"
+      
       end
       inner_counter += 1
       puts "inner #{inner_counter}"
     end
+    puts "min #{arr.push(min)} #{inner_counter}"
     counter += 1
   end
   arr
